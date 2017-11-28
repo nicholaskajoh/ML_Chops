e1b4794a2a86ba761081f9aee27c6afd31c5e316
Nicholas Kajoh <kajohterna@gmail.com>
2017-11-28 23:07:28 +0100

    use pandas to fetch data

--- linear-regression/main.py
+++ linear-regression/main.py
@@ -2,21 +2,9 @@ import pandas as pd
 import numpy as np
 from Linear_Regression import Linear_Regression
 
-# df = pd.read_csv("datasets/train.csv")
-# df.drop([''], 1, inplace=True)
-# print(df)
-
-# GET (TRAINING) DATA
-with open("datasets/train.csv", 'r') as tf:
-  xs, ys = [], []
-  data = tf.readlines()
-  for i in range(1, len(data)):
-    d = data[i].rstrip('\n')
-    _x, _y = [float(i) for i in d.split(',')]
-    xs.append(_x)
-    ys.append(_y)
-  xs = np.array(xs)
-  ys = np.array(ys)
+df = pd.read_csv("dataset/train.csv")
+xs = df['x']
+ys = df['y']
 
 lr = Linear_Regression(xs, ys)
 
